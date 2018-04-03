711b299951869f5c4e0a9b552af1741793b47cf7
unknown <adhirramjiawan0@gmail.com>
2018-04-03 14:58:10 +0200

    added placeholder assets for main menu and jumping between scenes:

--- MainMenuLogic.lua
+++ MainMenuLogic.lua
@@ -1,6 +1,11 @@
+local composer = require("composer")
+
 local MainMenuLogicModule = {}
 
 local background = nil
+local logo = nil
+local button_easy = nil
+local button_hard = nil
 local backgroundMusic = audio.loadSound("assets/music.ogg")
 
 local musicVolume = 0.5
@@ -31,13 +36,46 @@ local function keyboardListener(event)
     end
 end
 
+local function tapListener(event)
+
+    print ("click")
+ 
+    if (event.phase == "ended") then
+
+        if (event.x >= 250 and event.x <= 650 and event.y >= 400 and event.y <= 600) then
+            audio.stop(1)
+            composer.gotoScene("GameScene")
+        end
+
+    end
+    return true
+end
+
 function MainMenuLogicModule.CreateMainMenu()
    
     background = display.newImageRect("assets/background.jpg", 900, 900)
     background.anchorX = 0
     background.anchorY = 0
 
-    --background:addEventListener("touch", tapListener)
+    logo = display.newImageRect("assets/main_menu/logo.png", 600, 200)
+    logo.anchorX = 0
+    logo.anchorY = 0
+    logo.x = 150
+    logo.y = 50
+
+    button_easy = display.newImageRect("assets/main_menu/button_easy.png", 400, 200)
+    button_easy.anchorX = 0
+    button_easy.anchorY = 0
+    button_easy.x = 250
+    button_easy.y = 400
+
+    button_hard = display.newImageRect("assets/main_menu/button_hard.png", 400, 200)
+    button_hard.anchorX = 0
+    button_hard.anchorY = 0
+    button_hard.x = 250
+    button_hard.y = 650
+
+    background:addEventListener("touch", tapListener)
     Runtime:addEventListener("key", keyboardListener)
     --Runtime:addEventListener("enterFrame", onFrame)
 end
